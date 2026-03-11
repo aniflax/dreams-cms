62bb9f1ccba7cb2166fa4e4623aed652f8f75d89
𝐀𝐧𝐢𝐟𝐥𝐚𝐱 ツ <aniflax@Aniflaxs-MacBook-Air.local>
2026-03-12 02:00:53 +0530

    Update CMS CORS for custom frontend domains

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,20 +1,30 @@
-module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  {
-    name: 'strapi::cors',
-    config: {
-      origin: ['*'],
-      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
-      keepHeaderOnError: true,
+module.exports = ({ env }) => {
+  const allowedOrigins = [
+    env('FRONTEND_URL', 'https://dreamfurnitures.com'),
+    env('FRONTEND_WWW_URL', 'https://www.dreamfurnitures.com'),
+    env('FRONTEND_PREVIEW_URL'),
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+  ].filter(Boolean);
+
+  return [
+    'strapi::logger',
+    'strapi::errors',
+    'strapi::security',
+    {
+      name: 'strapi::cors',
+      config: {
+        origin: allowedOrigins,
+        methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+        headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+        keepHeaderOnError: true,
+      },
     },
-  },
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
+    'strapi::poweredBy',
+    'strapi::query',
+    'strapi::body',
+    'strapi::session',
+    'strapi::favicon',
+    'strapi::public',
+  ];
+};
