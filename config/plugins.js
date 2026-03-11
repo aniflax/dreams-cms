f65ee3c01c237ce223d78d648ea7ab0742178326
𝐀𝐧𝐢𝐟𝐥𝐚𝐱 ツ <aniflax@Aniflaxs-MacBook-Air.local>
2026-03-12 00:14:50 +0530

    Support standard Cloudinary environment variables

--- config/plugins.js
+++ config/plugins.js
@@ -1,7 +1,8 @@
 module.exports = ({ env }) => {
-  const hasCloudinaryConfig = ['CLOUDINARY_NAME', 'CLOUDINARY_KEY', 'CLOUDINARY_SECRET'].every(
-    (key) => env(key)
-  );
+  const cloudName = env('CLOUDINARY_NAME', env('CLOUDINARY_CLOUD_NAME'));
+  const apiKey = env('CLOUDINARY_KEY', env('CLOUDINARY_API_KEY'));
+  const apiSecret = env('CLOUDINARY_SECRET', env('CLOUDINARY_API_SECRET'));
+  const hasCloudinaryConfig = [cloudName, apiKey, apiSecret].every(Boolean);
 
   return {
     upload: {
@@ -9,9 +10,9 @@ module.exports = ({ env }) => {
         ? {
             provider: 'cloudinary',
             providerOptions: {
-              cloud_name: env('CLOUDINARY_NAME'),
-              api_key: env('CLOUDINARY_KEY'),
-              api_secret: env('CLOUDINARY_SECRET'),
+              cloud_name: cloudName,
+              api_key: apiKey,
+              api_secret: apiSecret,
             },
             actionOptions: {
               upload: {},
